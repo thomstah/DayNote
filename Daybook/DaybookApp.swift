a548ed5a12d6960b5b951677efc4fde0e958893a
thomstah <thommyxay@gmail.com>
2026-01-18 23:12:10 -0600

    Initial Commit

---
+++ Daybook/DaybookApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaybookApp.swift
+//  Daybook
+//
+//  Created by Thommy Xaysongkham on 1/18/26.
+//
+
+import SwiftUI
+
+@main
+struct DaybookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
