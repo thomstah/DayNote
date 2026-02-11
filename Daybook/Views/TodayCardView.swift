98d4912ab7c9cfb2ae8ca8e5d02c87fd4bdaf27c
thomstah <thommyxay@gmail.com>
2026-02-10 18:59:11 -0600

    working on home widgets

---
+++ Daybook/Views/TodayCardView.swift
@@ -0,0 +1,18 @@
+//
+//  TodayCardView.swift
+//  Daybook
+//
+//  Created by Thommy Xaysongkham on 2/10/26.
+//
+
+import SwiftUI
+
+struct TodayCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TodayCardView()
+}
