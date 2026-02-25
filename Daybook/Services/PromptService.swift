b61d5baf61c4894184997e9a6df334b5fe4218a7
thomstah <thommyxay@gmail.com>
2026-02-25 04:55:17 -0600

    on final stretch before submitting to student swift challenge. what left to do: - make "previous notes" view into a calendar instead of a vast list - scrap settings page for now - find a way to make the app look more lively and not lookl like it was made from apple lol

---
+++ Daybook/Services/PromptService.swift
@@ -0,0 +1,8 @@
+//
+//  PromptService.swift
+//  Daybook
+//
+//  Created by Thommy Xaysongkham on 2/25/26.
+//
+
+import Foundation
---
+++ Daybook/Views/PromptCardView.swift
@@ -0,0 +1,18 @@
+//
+//  PromptCardView.swift
+//  Daybook
+//
+//  Created by Thommy Xaysongkham on 2/25/26.
+//
+
+import SwiftUI
+
+struct PromptCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PromptCardView()
+}
